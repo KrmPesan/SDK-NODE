bd6bc3900df15fe56a93d84059a1f533a311611f
JuniYadi <juniyadi94@gmail.com>
2020-06-11 17:01:05 +0700

    Added Client Class

---
+++ src/Client.ts
@@ -0,0 +1,83 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+export class Client {
+    /**
+     * Default API Url.
+     */
+    protected apiUrl: string;
+
+    /**
+     * Default API Token.
+     */
+    protected token: string;
+
+    /**
+     * Default Timeout.
+     */
+    protected timeout: number;
+
+    /**
+     * Custom Request Header.
+     */
+    protected customHeader?: Object;
+
+    /**
+     * Construct Data to Class.
+     * 
+     * @param region string
+     * @param token string
+     * @param timeout number
+     * @param headers Object
+     */
+    constructor(
+        region: string,
+        token: string,
+        timeout?: number,
+        headers?: Object,
+    ) {
+        if(region == 'test') {
+            this.apiUrl = 'http://krmpesan.test';
+        } else {
+            this.apiUrl = `https://region${region}.krmpesan.com`;
+        }
+
+        this.token = token;
+        this.timeout = (timeout ?? 30) * 1000;
+        this.customHeader = headers;
+    }
+
+    action<T>(type: AxiosRequestConfig['method'], url: string, form?: Object): Promise<T> {
+        // Setup Url
+        const buildUrl: string = this.apiUrl + '/' + url;
+        
+        // set default header
+        let headers: Object;
+        headers = {
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + this.token
+        };
+
+        // use custom header if not null
+        if(this.customHeader) {
+            headers = this.customHeader;
+        }
+
+        // make default axios instance for request
+        const instance = {
+            url: buildUrl,
+            method: type,
+            headers: headers,
+            timeout: this.timeout,
+            data: form
+        }
+
+        return axios.request(instance)
+    }
+
+    /**
+     * Device Information.
+     */
+    deviceInfo() {
+        return this.action('GET', 'api/v2/device');
+    }
+}
